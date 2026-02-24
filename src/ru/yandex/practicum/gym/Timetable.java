7d9b6c35dbcccff84f03b0bd4b5f65f2fdf03e31
Shatura Sergey <serj.schatura@yandex.ru>
2026-02-24 21:32:29 +0300

    Сделал методы в Timetable

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,37 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private final HashMap <DayOfWeek, TreeMap <TimeOfDay, ArrayList<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+        if (timetable.containsKey(trainingSession.getDayOfWeek())) {
+            TreeMap<TimeOfDay, ArrayList<TrainingSession>> oldTreeMap = timetable.get(trainingSession.getDayOfWeek());
+            if (oldTreeMap.containsKey(trainingSession.getTimeOfDay())) {
+                ArrayList<TrainingSession> trainingSessions = oldTreeMap.get(trainingSession.getTimeOfDay());
+                trainingSessions.add(trainingSession);
+                oldTreeMap.put(trainingSession.getTimeOfDay(), trainingSessions);
+                timetable.put(trainingSession.getDayOfWeek(), oldTreeMap);
+            } else {
+                TreeMap<TimeOfDay, ArrayList<TrainingSession>> newTreeMap = new TreeMap<>();
+                ArrayList<TrainingSession> newTrainingSessions = new ArrayList<>();
+                newTrainingSessions.add(trainingSession);
+                newTreeMap.put(trainingSession.getTimeOfDay(), newTrainingSessions);
+                timetable.put(trainingSession.getDayOfWeek(), newTreeMap);
+            }
+        } else {
+            TreeMap<TimeOfDay, ArrayList<TrainingSession>> newTreeMap = new TreeMap<>();
+            ArrayList<TrainingSession> newTrainingSessions = new ArrayList<>();
+            newTrainingSessions.add(trainingSession);
+            newTreeMap.put(trainingSession.getTimeOfDay(), newTrainingSessions);
+            timetable.put(trainingSession.getDayOfWeek(), newTreeMap);
+        }
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public TreeMap<TimeOfDay, ArrayList<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        return timetable.get(dayOfWeek); //как реализовать, тоже непонятно, но сложность должна быть О(1)
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public ArrayList<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        return timetable.get(dayOfWeek).get(timeOfDay); //как реализовать, тоже непонятно, но сложность должна быть О(1)
     }
 }
