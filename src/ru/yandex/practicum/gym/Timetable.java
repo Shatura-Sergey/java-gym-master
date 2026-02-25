a417d9983e6a9e08759322246abcca867c2c0731
Shatura Sergey <serj.schatura@yandex.ru>
2026-02-25 16:38:59 +0300

    Исправил класс Timetable

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -1,37 +1,38 @@
 package ru.yandex.practicum.gym;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 import java.util.TreeMap;
 import java.util.Map;
 import java.util.NavigableSet;
 
 
+
 public class Timetable {
 
-    private final HashMap<DayOfWeek, TreeMap<TimeOfDay, ArrayList<TrainingSession>>> timetable = new HashMap<>();
+    private final HashMap<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
+    protected HashMap<Coach, Integer> coachesCounter = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        if (timetable.containsKey(trainingSession.getDayOfWeek())) {
-            TreeMap<TimeOfDay, ArrayList<TrainingSession>> oldTreeMap = timetable.get(trainingSession.getDayOfWeek());
-            if (oldTreeMap.containsKey(trainingSession.getTimeOfDay())) {
-                ArrayList<TrainingSession> trainingSessions = oldTreeMap.get(trainingSession.getTimeOfDay());
-                trainingSessions.add(trainingSession);
-                oldTreeMap.put(trainingSession.getTimeOfDay(), trainingSessions);
-                timetable.put(trainingSession.getDayOfWeek(), oldTreeMap);
-            } else {
-                ArrayList<TrainingSession> newTrainingSessions = new ArrayList<>();
-                newTrainingSessions.add(trainingSession);
-                oldTreeMap.put(trainingSession.getTimeOfDay(), newTrainingSessions);
-                timetable.put(trainingSession.getDayOfWeek(), oldTreeMap);
-            }
-        } else {
-            TreeMap<TimeOfDay, ArrayList<TrainingSession>> newTreeMap = new TreeMap<>();
-            ArrayList<TrainingSession> newTrainingSessions = new ArrayList<>();
-            newTrainingSessions.add(trainingSession);
-            newTreeMap.put(trainingSession.getTimeOfDay(), newTrainingSessions);
-            timetable.put(trainingSession.getDayOfWeek(), newTreeMap);
+        DayOfWeek dayOfWeek = trainingSession.getDayOfWeek();
+        TimeOfDay timeOfDay = trainingSession.getTimeOfDay();
+
+        TreeMap<TimeOfDay, List<TrainingSession>> trainingsForDay = timetable.get(dayOfWeek);
+        if (trainingsForDay == null) {
+            trainingsForDay = new TreeMap<>();
+            timetable.put(dayOfWeek, trainingsForDay);
         }
+
+        List<TrainingSession> trainingSessions = trainingsForDay.get(timeOfDay);
+        if (trainingSessions == null) {
+            trainingSessions = new ArrayList<>();
+            trainingsForDay.put(timeOfDay, trainingSessions);
+        }
+
+        trainingSessions.add(trainingSession);
+        Coach currentCoach = trainingSession.getCoach();
+        coachesCounter.put(currentCoach, coachesCounter.getOrDefault(currentCoach, 0) + 1);
     }
 
     public NavigableSet<TimeOfDay> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
@@ -42,9 +43,9 @@ public class Timetable {
         }
     }
 
-    public ArrayList<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         if (timetable.containsKey(dayOfWeek)) {
-            TreeMap<TimeOfDay, ArrayList<TrainingSession>> treeMap = timetable.get(dayOfWeek);
+            TreeMap<TimeOfDay, List<TrainingSession>> treeMap = timetable.get(dayOfWeek);
             if (treeMap.containsKey(timeOfDay)) {
                 return treeMap.get(timeOfDay);
             }
@@ -52,19 +53,9 @@ public class Timetable {
         return new ArrayList<>();
     }
 
-    public ArrayList<Map.Entry<String, Integer>> getCountByCoaches() {
-        HashMap<String, Integer> coachers = new HashMap<>();
-
-        for (TreeMap<TimeOfDay, ArrayList<TrainingSession>> dayMap : timetable.values()) {
-            for (ArrayList<TrainingSession> sessions : dayMap.values()) {
-                for (TrainingSession session : sessions) {
-                    String coachName = session.getCoach().toString();
-                    coachers.put(coachName, coachers.getOrDefault(coachName, 0) + 1);
-                }
-            }
-        }
-        ArrayList<Map.Entry<String, Integer>> lessons = new ArrayList<>(coachers.entrySet());
-        lessons.sort((lesson1, lesson2) -> lesson2.getValue().compareTo(lesson1.getValue()));
-        return lessons;
+    public List<Map.Entry<Coach, Integer>> getCountByCoaches() {
+        List<Map.Entry<Coach, Integer>> result = new ArrayList<>(coachesCounter.entrySet());
+        result.sort((a, b) -> b.getValue().compareTo(a.getValue())); // Сортировка по количеству тренировок в порядке убывания
+        return result;
     }
 }
